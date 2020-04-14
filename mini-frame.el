b30c403451421c23fc8a9db3bc474e342e049d44
muffinmad <andreyk.mad@gmail.com>
2020-04-14 11:32:22 +0300

    Make edebug-eval-expression string
    Without requiring `'edebug` customization interface of ignored commands is broken

--- mini-frame.el
+++ mini-frame.el
@@ -68,7 +68,7 @@
   "Show minibuffer in child frame."
   :group 'minibuffer)
 
-(defcustom mini-frame-ignore-commands '(eval-expression edebug-eval-expression debugger-eval-expression)
+(defcustom mini-frame-ignore-commands '(eval-expression "edebug-eval-expression" debugger-eval-expression)
   "For this commands minibuffer will not be displayed in child frame."
   :type '(repeat (choice function regexp)))
 
