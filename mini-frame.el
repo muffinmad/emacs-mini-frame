6716b6cff2e262d7ca6018467a2730caee78fdbf
gcv <vetoshev@gmail.com>
2020-04-14 01:28:07 -0700

    Add support for regex values to mini-frame-ignore-commands. (#7)

--- mini-frame.el
+++ mini-frame.el
@@ -70,7 +70,7 @@
 
 (defcustom mini-frame-ignore-commands '(eval-expression edebug-eval-expression debugger-eval-expression)
   "For this commands minibuffer will not be displayed in child frame."
-  :type '(repeat function))
+  :type '(repeat (choice function regexp)))
 
 (defcustom mini-frame-show-parameters '((left . 0.5)
                                         (top . 0)
@@ -273,7 +273,12 @@ ALIST is passed to `window--display-buffer'."
   "Show minibuffer-only child frame (if needed) and call FN with ARGS."
   (cond
    ((or (minibufferp)
-        (memq this-command mini-frame-ignore-commands))
+        (catch 'ignored
+          (dolist (ignored-command mini-frame-ignore-commands)
+            (when (if (stringp ignored-command)
+                      (string-match-p ignored-command (symbol-name this-command))
+                    (eq ignored-command this-command))
+              (throw 'ignored t)))))        
     (apply fn args))
    ((and (frame-live-p mini-frame-frame)
          (frame-visible-p mini-frame-frame))
