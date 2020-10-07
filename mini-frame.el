29606accd6cc8fd89733368d0e9235f8a6ac5eaf
muffinmad <andreyk.mad@gmail.com>
2020-10-07 21:34:23 +0300

    Add 'unspecified to internal-border-color choices

--- mini-frame.el
+++ mini-frame.el
@@ -93,9 +93,10 @@ Called if `mini-frame-show-parameters' doesn't specify background color."
   :type 'function)
 
 (defcustom mini-frame-internal-border-color nil
-  "MiniFrame internal border color."
-   :type '(choice (const :tag "None" nil)
-                  (color :tag "Color")))
+  "When set, set the internal border color of mini-frames to this color."
+   :type '(choice (const :tag "Not set" nil)
+                  (color :tag "Color")
+                  (const :tag "Unspecified" 'unspecified)))
 
 (defcustom mini-frame-handle-completions t
   "Create child frame to display completions buffer."
