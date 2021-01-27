a71af2dc256895c167a2f93d03283a39be5e78e7
Steven Allen <steven@stebalien.com>
2021-01-27 12:09:27 -0800

    save window-excursion & current-buffer (#29)
    1. Point is no longer restored when exiting `mini-frame`, just the
    current buffer.
    2. The window configuration _is_ restored.

--- mini-frame.el
+++ mini-frame.el
@@ -358,8 +358,7 @@ ALIST is passed to `window--display-buffer'."
    ((and (frame-live-p mini-frame-frame)
          (frame-parameter mini-frame-frame 'parent-frame)
          (frame-visible-p mini-frame-frame))
-    (save-excursion
-      (mini-frame--display fn args)))
+    (mini-frame--display fn args))
    (t
     ;; On windows `frame-visible-p' can be t even if the frame is not visible so
     ;; calling `make-frame-visible' doesn't make frame actually visible.  Make frame
@@ -390,17 +389,18 @@ ALIST is passed to `window--display-buffer'."
       (ignore ivy-fixed-height-minibuffer)
       (ignore resize-mini-frames)
       (ignore which-key-popup-type)
-      (save-excursion
-        (unwind-protect
-            (mini-frame--display fn args)
-          (when (frame-live-p mini-frame-completions-frame)
-            (make-frame-invisible mini-frame-completions-frame))
-          (when (frame-live-p mini-frame-selected-frame)
-            (select-frame-set-input-focus mini-frame-selected-frame))
-          (when (frame-live-p mini-frame-frame)
-            (make-frame-invisible mini-frame-frame)
-            (when mini-frame-detach-on-hide
-              (modify-frame-parameters mini-frame-frame '((parent-frame . nil)))))))))))
+      (save-current-buffer
+        (save-window-excursion
+          (unwind-protect
+              (mini-frame--display fn args)
+            (when (frame-live-p mini-frame-completions-frame)
+              (make-frame-invisible mini-frame-completions-frame))
+            (when (frame-live-p mini-frame-selected-frame)
+              (select-frame-set-input-focus mini-frame-selected-frame))
+            (when (frame-live-p mini-frame-frame)
+              (make-frame-invisible mini-frame-frame)
+              (when mini-frame-detach-on-hide
+                (modify-frame-parameters mini-frame-frame '((parent-frame . nil))))))))))))
 
 (defun mini-frame--advice (funcs func &optional remove)
   "Add advice FUNC around FUNCS.  If REMOVE, remove advice instead."
